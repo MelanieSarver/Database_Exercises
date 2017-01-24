a00a25db0f81dbe4e94f74c26fb03e16b8db476c
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-24 15:17:49 -0600

    Exercise 6.4.2
    refactored for greater readability.

--- src/select_exercise.sql
+++ src/select_exercise.sql
@@ -1,20 +1,26 @@
 USE codeup_test_db;
 
-SELECT name FROM albums
+SELECT name
+FROM albums
 WHERE artist = 'Pink Floyd';
 
-SELECT release_date from albums
+SELECT release_date
+FROM albums
 WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
 
-SELECT genre from albums
+SELECT genre
+FROM albums
 WHERE name = 'Nevermind';
 
-SELECT name from albums
-where release_date between 1990 AND 1999;
+SELECT name
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT name from albums
-where sales < 20000000;
+SELECT name
+FROM albums
+WHERE sales < 20000000;
 
-SELECT name FROM albums
+SELECT name
+FROM albums
 WHERE genre LIKE '%rock%';
 
