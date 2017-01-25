541429a00c6b84856144f34a1d82cefd2684f86d
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-25 15:34:36 -0600

    Exercise 6.8.5
    2. complete
    3. complete

--- src/limit_exercises.sql
+++ src/limit_exercises.sql
@@ -3,3 +3,10 @@ FROM employees
 ORDER BY last_name DESC
 Limit 10;
 
+SELECT first_name, last_name
+FROM employees
+WHERE (hire_date LIKE '199%'
+       AND birth_date LIKE '%12-25')
+ORDER BY birth_date, hire_date DESC
+Limit 5;
+
