546e5b9610bdf87e37063dbff392879aacb4e9cb
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-25 14:51:16 -0600

    Exercise 6.8.4
    2. complete
    3. complete
    4. complete
    5. complete

--- src/group_by_exercise.sql
+++ src/group_by_exercise.sql
@@ -2,7 +2,8 @@ SELECT DISTINCT title
 FROM titles
 ORDER BY title ASC;
 
-SELECT last_name
+SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'e%e'
-GROUP BY last_name;
+GROUP BY last_name, first_name;
+
