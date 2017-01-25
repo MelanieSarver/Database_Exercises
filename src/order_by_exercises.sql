633c9dd042cfe0e23196b1e2ebe00074baa70903
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-25 13:52:53 -0600

    Exercise 6.8.3
    2. complete
    3. complete
    4. complete
    5. complete
    6. complete
    7. complete

--- src/order_by_exercises.sql
+++ src/order_by_exercises.sql
@@ -15,7 +15,8 @@ ORDER BY emp_no DESC;
 SELECT *
 FROM employees
 WHERE (hire_date LIKE '199%'
-       AND birth_date LIKE '%12-25');
+       AND birth_date LIKE '%12-25')
+ORDER BY birth_date, hire_date DESC;
 
 SELECT *
 FROM employees
