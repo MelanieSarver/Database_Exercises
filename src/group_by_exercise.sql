f04190e7e4cf72fd4b441ac064456c6843b53eed
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-25 14:52:23 -0600

    Exercise 6.8.4
    2. complete
    3. complete
    4. complete
    5. complete
    6. complete

--- src/group_by_exercise.sql
+++ src/group_by_exercise.sql
@@ -7,3 +7,7 @@ FROM employees
 WHERE last_name LIKE 'e%e'
 GROUP BY last_name, first_name;
 
+SELECT DISTINCT last_name
+FROM employees
+WHERE (last_name LIKE '%q%'
+       AND NOT last_name LIKE '%qu%');
