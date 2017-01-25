672dfd4074a275a0c34a31860afcecf5a70ff805
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-25 13:44:52 -0600

    Exercise 6.8.3
    2. complete

---
+++ src/order_by_exercises.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena'
+       OR first_name = 'Vidya'
+       OR first_name = 'Maya')
+      AND gender = 'm'
+ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE (last_name LIKE 'e%'
+       AND last_name LIKE '%e');
+
+SELECT *
+FROM employees
+WHERE (hire_date LIKE '199%'
+       AND birth_date LIKE '%12-25');
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+SELECT *
+FROM employees
+WHERE (last_name LIKE '%q%'
+       AND NOT last_name LIKE '%qu%');
