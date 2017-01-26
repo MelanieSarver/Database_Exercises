0ffc67be43bd9b860cbcb8bc03861e259aafbbf8
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-26 16:57:17 -0600

    Exercise 6.11.1
    Employees Database
    1. complete
    2. complete
    3. complete

--- src/joint_test_exercise.sql
+++ src/joint_test_exercise.sql
@@ -23,6 +23,9 @@ USE employees;
 SELECT *
 FROM dept_manager;
 
+SELECT *
+FROM titles;
+
 SELECT dept_name AS 'Department Name', concat(first_name, ' ', last_name) AS 'Department Manager'
 FROM employees e
   JOIN dept_manager ON e.emp_no = dept_manager.emp_no
@@ -34,4 +37,14 @@ FROM employees e
   JOIN dept_manager ON e.emp_no = dept_manager.emp_no
   Join departments ON dept_manager.dept_no = departments.dept_no
 WHERE dept_manager.to_date > curdate() AND e.gender = 'F';
+
+Select title AS 'Title', count(employees.first_name) AS 'Count'
+FROM titles
+Join employees ON titles.emp_no = employees.emp_no
+  JOIN dept_emp on employees.emp_no = dept_emp.emp_no
+  join departments ON dept_emp.dept_no = departments.dept_no
+Where departments.dept_name = 'Customer Service' AND titles.to_date > curdate()
+GROUP BY title;
+
+
 
