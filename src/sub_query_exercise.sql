c26a8e25aa71d427514cf8f4c892c7f3f249b040
Melanie Sarver <melanie.sarver@gmail.com>
2017-01-27 11:23:02 -0600

    Exercise 6.11.3
    Employees Database
    2. a) complete
        b) complete

--- src/sub_query_exercise.sql
+++ src/sub_query_exercise.sql
@@ -7,3 +7,12 @@ WHERE hire_date IN (
   FROM employees
   WHERE emp_no = 101010
 );
+
+
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE first_name = 'Aamod'
+);
